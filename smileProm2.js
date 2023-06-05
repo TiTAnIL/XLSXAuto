fad212d1a312cc9d315f70b54ccefcb23815d395
Denis <denismagin@gmail.com>
2023-06-05 14:18:42 +0300

    fixed "remove duplicates" logic
    changed the final file processing

--- smileProm2.js
+++ smileProm2.js
@@ -1,17 +1,26 @@
+function downloadFile(data, filename) {
+  var element = document.createElement('a');
+  element.setAttribute('href', data);
+  element.setAttribute('download', filename);
+  element.style.display = 'none';
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
+}
+
 function processFile() {
   var fileInput = document.getElementById('file-input');
   var file = fileInput.files[0];
-
   var reader = new FileReader();
   reader.onload = function (e) {
     var data = new Uint8Array(e.target.result);
     var workbook = XLSX.read(data, { type: 'array' });
-
+    
     // Step 1: Iterate over each sheet in the workbook
     for (var sheetName in workbook.Sheets) {
       if (workbook.Sheets.hasOwnProperty(sheetName)) {
         var sheet = workbook.Sheets[sheetName];
-
+        
         // Step 2: Convert the sheet to JSON format
         var jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
 
@@ -42,66 +51,63 @@ function processFile() {
           }
         });
 
-        // Step 5: Process each row in the sheet
-// Step 5: Process each row in the sheet
-var modifiedRows = [];
-var deletedRows = [];
+        var uniqueValues = new Set();
+        var modifiedRows = [];
+        var deletedRows = [];
 
-for (var i = 1; i < jsonData.length; i++) {
-  var row = jsonData[i];
-  var hasValue = false;
+        for (var i = 1; i < jsonData.length; i++) {
+          var row = jsonData[i];
+          var hasValue = false;
 
-  // Check if any cell under the column "הוכנס טיפול" has a value
-  if (row[colTreat]) {
-    hasValue = true;
-  }
+          // Check if any cell under the column "הוכנס טיפול" has a value
+          if (row[colTreat]) {
+            hasValue = true;
+          }
 
-  // Check if any cell under the column "מספר אתר לפניה" is not unique
-  var siteNumInq = row[colSiteNumInq];
-  var isUnique = true;
+          // Check if the value under the column "מספר אתר לפניה" is a duplicate
+          var siteNumInq = row[colSiteNumInq];
 
-  for (var j = i + 1; j < jsonData.length; j++) {
-    var nextRow = jsonData[j];
-    if (nextRow[colSiteNumInq] === siteNumInq) {
-      isUnique = false;
-      break;
-    }
-  }
+          if (!uniqueValues.has(siteNumInq)) {
+            if (hasValue) {
+              deletedRows.push(row); // Add the row to the deleted rows array
+            } else {
+              uniqueValues.add(siteNumInq); // Add the value to the set of unique values
 
-  if (hasValue || !isUnique) {
-    deletedRows.push(row); // Add the row to the deleted rows array
-  } else {
-    var modifiedRow = {};
+              var modifiedRow = {};
 
-    modifiedRow['הוכנס טיפול'] = row[colTreat];
-    modifiedRow['מספר אתר לפניה'] = row[colSiteNumInq];
-    modifiedRow['מספר אתר'] = row[colSiteNum];
-    modifiedRow['תז נציג יוצר פניה'] = row[colRepId];
-    modifiedRow['מספר פניה'] = row[colInqNum];
+              modifiedRow['הוכנס טיפול'] = row[colTreat];
+              modifiedRow['מספר אתר לפניה'] = row[colSiteNumInq];
+              modifiedRow['מספר אתר'] = row[colSiteNum];
+              modifiedRow['תז נציג יוצר פניה'] = row[colRepId];
+              modifiedRow['מספר פניה'] = row[colInqNum];
 
-    modifiedRows.push(modifiedRow);
-  }
-}
+              modifiedRows.push(modifiedRow);
+            }
+          }
+        }
 
-// Log the deleted rows
-console.log('Deleted Rows:', deletedRows);
+        // Log the deleted rows
+        console.log('Deleted Rows:', deletedRows);
 
-        // Step 6: Check if there are any modified rows
+        // Log the modified rows
+        console.log('Modified Rows:', modifiedRows);
+
+        // Step 9: Save each modified sheet as a separate file
         if (modifiedRows.length > 0) {
-          // Step 7: Create a new workbook and sheet
           var newWorkbook = XLSX.utils.book_new();
           var newSheet = XLSX.utils.json_to_sheet(modifiedRows, {
             header: ['הוכנס טיפול', 'מספר אתר לפניה', 'מספר אתר', 'תז נציג יוצר פניה', 'מספר פניה']
           });
 
-          // Step 8: Add the sheet to the workbook
           XLSX.utils.book_append_sheet(newWorkbook, newSheet, sheetName);
-
-          // Step 9: Save the new workbook as a new file
-          XLSX.writeFile(newWorkbook, sheetName + '.xlsx');
+          var newWorkbookData = XLSX.write(newWorkbook, { bookType: 'xlsx', type: 'array' });
+          var blob = new Blob([newWorkbookData], { type: 'application/octet-stream' });
+          var url = URL.createObjectURL(blob);
+          downloadFile(url, sheetName + '.xlsx');
         }
       }
     }
   };
+
   reader.readAsArrayBuffer(file);
 }
