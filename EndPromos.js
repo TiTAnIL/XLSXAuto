89f6b52f9ffc219e0cf21b6950f6b1ee519ca563
Denis <denismagin@gmail.com>
2023-07-31 16:36:51 +0300

    started end promos procedure

--- EndPromos.js
+++ EndPromos.js
@@ -1,89 +1,85 @@
+// Entry point of the process
+async function processEndPromos(files, cycle) {
 
-// Load File 1 button
-document.getElementById("file1-btn").addEventListener("click", function() {
-  loadExcelFile("file1.xlsx", function(data) {
-    // Do something with the data, e.g. extract a column
-    var column = extractColumn(data, "Column1");
-    console.log(column);
-  });
-});
+  // Load and parse the data from each file
+  // loop over files and load each one, the first file is r389, the second is index, the third is codes
+  // load the files using the loadAndParseFile function
 
-// Load File 2 button
-document.getElementById("file2-btn").addEventListener("click", function() {
-  loadExcelFile("file2.xlsx", function(data) {
-    // Do something with the data, e.g. filter rows
-    var filteredData = filterRows(data, function(row) {
-      return row["Column2"] === "Value1";
-    });
-    console.log(filteredData);
-  });
-});
+  const r389Data = await loadAndParseFile(files[0]);
+  const indexData = await loadAndParseFile(files[1]);
+  const codesData = await loadAndParseFile(files[2]);
 
-// Load File 3 button
-document.getElementById("file3-btn").addEventListener("click", function() {
-  loadExcelFile("file3.xlsx", function(data) {
-    // Do something with the data, e.g. perform a VLOOKUP
-    var lookupValue = "Value1";
-    var lookupResult = vlookup(data, "Column1", "Column2", lookupValue);
-    console.log(lookupResult);
-  });
-});
-
-// Load an Excel file and parse it into a 2D array
-function loadExcelFile(filename, callback) {
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", filename, true);
-  xhr.responseType = "arraybuffer";
-  xhr.onload = function(e) {
-    var arrayBuffer = xhr.response;
-    var data = parseExcelFile(arrayBuffer);
-    callback(data);
+  // Define an object to store the required column indexes
+  const columnIndexes = {
+    r389AccountNumber: findColumnIndex(r389Data, 'Account Number'),
+    r389DataPromotionCode: findColumnIndex(r389Data, 'Promotion Code'),
+    r398PromoExpiteDate: findColumnIndex(r389Data, 'Promo Expire Date'),
+    r389PromotionSeq: findColumnIndex(r389Data, 'Promotion Seq'),
+    r389BillingCycle: findColumnIndex(r389Data, 'Billing Cycle'),
+    indexDataPromotionCode: findColumnIndex(indexData, 'PROMOTION_CODE'),
+    indexDataTreatMethod: findColumnIndex(indexData, 'אופן טיפול- מבצע'),
+    indexDataExtantionPeriod: findColumnIndex(indexData, 'תקופת הארכה'),
+    codesDataPromotionCode: findColumnIndex(codesData, 'Promotion Code'),
+    codesDataNewPromoCode: findColumnIndex(codesData, 'קוד מבצע חדש')
   };
-  xhr.send();
+
+
+  // Compare data points from different objects and store the crossed data to a new object
+  const crossedData = compareData(r389Data, indexData, codesData, columnIndexes);
+
+  // Save the crossed data to a new file
+  await saveToFile(crossedData, 'output.xlsx');
 }
 
-// Parse an Excel file (in XLSX format) into a 2D array
-function parseExcelFile(arrayBuffer) {
-  var workbook = XLSX.read(arrayBuffer, {type:"array"});
-  var worksheet = workbook.Sheets[workbook.SheetNames[0]];
-  var data = XLSX.utils.sheet_to_json(worksheet, {header:1});
-  return data;
+// Helper functions
+async function loadAndParseFile(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      const data = new Uint8Array(e.target.result);
+      const workbook = XLSX.read(data, { type: 'array' });
+      const sheetName = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[sheetName];
+      const parsedData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+      resolve(parsedData);
+    };
+    reader.onerror = function (error) {
+      reject(error);
+    };
+    reader.readAsArrayBuffer(file);
+  });
 }
 
-// Extract a column from an Excel data array
-function extractColumn(data, columnName) {
-  var column = [];
-  var headerRow = data[0];
-  var columnIndex = headerRow.indexOf(columnName);
-  if (columnIndex >= 0) {
-    for (var i = 1; i < data.length; i++) {
-      column.push(data[i][columnIndex]);
-    }
+function handleCycle(event) {
+	const selectedOption = event.target.value;
+	console.log("Chosen Option:", selectedOption);
+}
+
+function findColumnIndex(data, column) {
+  try {
+    const row = data[0];
+    const index = row.findIndex((cell) => cell === column);
+    if (index === -1) {
+      throw new Error(`Column ${column} not found`);
+    } // else {
+    //  console.log(`Column ${column} found at index ${index}`);
+    // }
+    return index;
+  } catch (error) {
+    console.error('Error:', error, '\n', 'Column:', column, '\n', 'Data:', data);
   }
-  return column;
+
 }
 
-// Filter rows in an Excel data array based on a filter function
-function filterRows(data, filterFunction) {
-  var filteredData = [data[0]];
-  for (var i = 1; i < data.length; i++) {
-    var row = data[i];
-    if (filterFunction(row)) {
-      filteredData.push(row);
-    }
-  }
-  return filteredData;
+function compareData(r389Data, indexData, codesData, columnIndexes) {
+  // Compare data points from the different objects and return the crossed data as a new object
+  // create an empty array to store the crossed data
+  // do an equelent to excel vlookup function on the r389Data and indexData by the promotion code and if there is a match, add to the array the data from the r389Data and the indexData.
+  // the crossed data array should look like this:
+  // r389AccountNumber, r389DataPromotionCode, r389PromotionSeq, r389Bi
+
 }
 
-// Perform a VLOOKUP-like operation on an Excel data array
-function vlookup(data, lookupColumn, resultColumn, lookupValue) {
-  var headerRow = data[0];
-  var lookupIndex
-}
-
-const clearFilesBtn = document.getElementById('clearFilesBtn')
-clearFilesBtn.addEventListener('click'), function() {
-document.getElemntById('file1').value = ''
-document.getElemntById('file2').value = ''
-document.getElemntById('file3').value = ''
+async function saveToFile(data, filename) {
+  // Save the data to a new file using xlsx
 }
